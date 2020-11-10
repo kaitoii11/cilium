9322e76a937bc4074227d528e0a8f03a32347e4c
Robin Hahling <robin.hahling@gw-computing.net>
2020-11-10 16:27:11 +0100

    api/observer: re-generate protobuf code
    Signed-off-by: Robin Hahling <robin.hahling@gw-computing.net>

--- api/v1/observer/observer.pb.json.go
+++ api/v1/observer/observer.pb.json.go
@@ -46,6 +46,24 @@ func (msg *ServerStatusResponse) UnmarshalJSON(b []byte) error {
 }
 
 // MarshalJSON implements json.Marshaler
+func (msg *Node) MarshalJSON() ([]byte, error) {
+	var buf bytes.Buffer
+	err := (&jsonpb.Marshaler{
+		EnumsAsInts:  false,
+		EmitDefaults: false,
+		OrigName:     true,
+	}).Marshal(&buf, msg)
+	return buf.Bytes(), err
+}
+
+// UnmarshalJSON implements json.Unmarshaler
+func (msg *Node) UnmarshalJSON(b []byte) error {
+	return (&jsonpb.Unmarshaler{
+		AllowUnknownFields: false,
+	}).Unmarshal(bytes.NewReader(b), msg)
+}
+
+// MarshalJSON implements json.Marshaler
 func (msg *GetFlowsRequest) MarshalJSON() ([]byte, error) {
 	var buf bytes.Buffer
 	err := (&jsonpb.Marshaler{
